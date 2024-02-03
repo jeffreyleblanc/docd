98aca56cd40065e21e3a241d2e75e551b92adf39
Jeff LeBlanc <jeffreyleblanc@users.noreply.github.com>
2024-02-02 21:35:13 -0500

    Initial deep source unpacking.

--- docd/publisher.py
+++ docd/publisher.py
@@ -19,6 +19,7 @@ class Category:
     kind =  "category"
     name:   str = None
     uri:    str = None
+    # parent_path
 
 @dataclass
 class FileRep:
@@ -27,7 +28,22 @@ class FileRep:
     source: Path = None
     dest:   Path = None
     uri:    str = None
+    # parent_path
 
+
+FILE_MAP = {
+    ".md":"markdown",
+    ".py":"python",
+    ".txt":"",
+    ".html":"html",
+    ".sh":"bash",
+    ".bash":"bash",
+    ".vue":"html",
+    ".js":"javascript",
+    ".css":"css"
+}
+
+SKIP_DIRECTORIES = (".git","_output","_media")
 
 class Publisher:
 
@@ -93,6 +109,72 @@ class Publisher:
                 print(c,o,e)
                 raise Exception("Rsync of _media failed")
 
+        print("Begin new file parser")
+
+        self.flat_file_map = {}
+
+        def _unpack_dirs(SOURCE_ROOT, depth=0, max_depth=100000):
+            indent = " "*(4*depth)
+            print(f"{indent}UNPACK",SOURCE_ROOT.name,depth)
+
+            node = {
+                "depth": depth,
+                "kind": "directory",
+                "path": str(SOURCE_ROOT.relative_to(self.SOURCE_ROOT)),
+                "name": SOURCE_ROOT.name,
+                "files": [],
+                "directories": []
+            }
+
+            # Traverse the sources
+            srcs = sorted([s for s in SOURCE_ROOT.iterdir()])
+            for path in srcs:
+                # Ignore skipped directories (Note should make deep path support)
+                if path.is_dir() and path.name in SKIP_DIRECTORIES:
+                    continue
+
+                relpath = path.relative_to(self.SOURCE_ROOT)
+
+                if path.is_file():
+                    uriupp = relpath.parent
+                    uri = str(uriupp/path.stem)
+                    if path.suffix != ".md":
+                        uri += f"--dot-{path.suffix[1:]}"
+
+                    fnode = {
+                        "depth": depth,
+                        "kind": "file",
+                        "path": str(relpath),
+                        "name": path.name,
+                        "stem": path.stem,
+                        "suffix": path.suffix,
+                        "db_file_path": None,
+                        "uri": uri
+                    }
+                    node["files"].append(fnode)
+                    self.flat_file_map[str(relpath)] = fnode
+                elif path.is_dir() and depth+1 <= max_depth:
+                    subnode = _unpack_dirs(path,depth=depth+1,max_depth=max_depth)
+                    node["directories"].append(subnode)
+
+            return node
+
+
+        root_node = _unpack_dirs(self.SOURCE_ROOT,max_depth=2)
+        print("-"*32)
+        print(json.dumps(root_node,indent=4))
+        print(json.dumps(self.flat_file_map,indent=4))
+
+        return
+
+        ## Reset Paths #########################################
+
+        self.all_paths_list = []
+
+        ## Process the Directories #########################################
+
+        # Break this into chunks
+
         # Make the main page
         main_page = self.SOURCE_ROOT/"__main__.md"
         if main_page.is_file():
@@ -104,28 +186,19 @@ class Publisher:
             f.write(main_html)
 
         # Traverse the sources
-        self.all_paths_list = []
         srcs = sorted([s for s in self.SOURCE_ROOT.iterdir()])
         for srcdir in srcs:
             # Ignore files in root directory
             if not srcdir.is_dir():
                 continue
             # Ignore more files
-            if srcdir.name in (".git","_output","_media"):
+            if srcdir.name in SKIP_DIRECTORIES:
                 continue
 
             # Render found files
-            self._render_sources(srcdir,srcdir.name,{
-                ".md":"markdown",
-                ".py":"python",
-                ".txt":"",
-                ".html":"html",
-                ".sh":"bash",
-                ".bash":"bash",
-                ".vue":"html",
-                ".js":"javascript",
-                ".css":"css"
-            })
+            self._process_directory(srcdir,srcdir.name,FILE_MAP)
+
+        ## Make database #########################################
 
         # Make the index database
         page_database = []
@@ -151,7 +224,7 @@ class Publisher:
 
     #-- Helpers ----------------------------------------------------------------#
 
-    def _render_sources(self, source_directory, out_uri, suffix_lang_map):
+    def _process_directory(self, source_directory, out_uri, suffix_lang_map):
         # Make the target directory
         TGT = self.DEST_ROOT_DB/out_uri
         TGT.mkdir(exist_ok=True)
@@ -175,13 +248,13 @@ class Publisher:
         for item in sources:
             # Add the contents
             language = suffix_lang_map.get(item.source.suffix)
-            content = self._render_contents(item.source,language)
+            content = self._render_page(item.source,language)
 
             # Write the file
             with item.dest.open("w") as f:
                 f.write(content)
 
-    def _render_contents(self, source_path, language):
+    def _render_page(self, source_path, language):
         if language == "markdown":
             return make_html(source_path.read_text())
         else:
