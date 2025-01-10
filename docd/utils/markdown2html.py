af40ad2de00cc6e70e8bb0e30b387a32233db858
Jeff LeBlanc <jeffreyleblanc@users.noreply.github.com>
2025-01-09 21:37:12 -0500

    Update markdown notes.

--- docd/utils/markdown2html.py
+++ docd/utils/markdown2html.py
@@ -4,6 +4,25 @@
 import markdown
 
 
+"""
+# To use 'Markdown' library
+
+While we use `mistune` in `docd`, if you want to switch to `markdown`,
+the rough implementation looks like:
+
+```python
+from markdown import markdown
+def make_html(text):
+    config = {
+        'output_format': 'html5',
+        'extensions': ['fenced_code','codehilite'],
+        'extension_configs': {
+            'codehilite': { 'guess_lang': False, 'css_class': 'highlighted-syntax' }
+        }}
+    return markdown(text,**config)
+```
+"""
+
 def make_html(str_src):
     return markdown.markdown(str_src, extensions=[
         "markdown.extensions.extra",
